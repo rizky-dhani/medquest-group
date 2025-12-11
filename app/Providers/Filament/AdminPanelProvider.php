eb3bd496010a546b0362012a3f1a6671619cfd8e
Rizky Dhani <rizkydhani15@gmail.com>
2025-12-11 16:55:02 +0700

    chore(config): update admin panel path and add trust proxies middleware

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -34,7 +34,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('')
+            ->path('/')
             ->spa()
             ->login()
             ->profile()
